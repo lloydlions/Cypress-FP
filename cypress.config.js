52b316e27efdc82f849fc29cc228204a7368950a
Lloyd Lions <lloyd.lions@gmail.com>
2023-06-22 17:39:59 +0800

    fix testdata

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
   },
   e2e: {
     specPattern: "cypress/e2e/*/*.spec.js",
+    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
